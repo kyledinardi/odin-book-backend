1469a5d9dbcb84fa5e7081708378b4a24198551d
kyledinardi <94251418+kyledinardi@users.noreply.github.com>
2024-09-28 13:04:51 -0400

    Add post creation

---
+++ controllers/postController.js
@@ -0,0 +1,27 @@
+// const multer = require('multer');
+const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
+// const { unlink } = require('fs/promises');
+// const cloudinary = require('cloudinary').v2;
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+exports.createPost = [
+  body('text', 'Post text must not be empty').trim().notEmpty(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const { text } = req.body;
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ text, errors: errors.array() });
+    }
+
+    const post = await prisma.post.create({
+      data: { text, author: { connect: { id: req.user.id } } },
+    });
+
+    return res.json({ post });
+  }),
+];
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const passport = require('passport');
 const userController = require('../controllers/userController');
+const postController = require('../controllers/postController');
 
 const router = express.Router();
 
 router.post('/users', userController.createUser);
 router.post('/users/login', userController.login);
 router.use(passport.authenticate('jwt', { session: false }));
+
+router.post('/posts', postController.createPost);
 
 module.exports = router;
