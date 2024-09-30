0b86ddd966a8a50314604cdeee11d2c8924e62f5
kyledinardi <94251418+kyledinardi@users.noreply.github.com>
2024-09-30 13:31:23 -0400

    Add cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 require('./passport');
 const express = require('express');
+const cors = require('cors');
 const indexRouter = require('./routes/index');
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/', indexRouter);
