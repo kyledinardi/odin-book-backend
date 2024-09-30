fc1e6d1429f4df34dba67153f07d75c99a76542e
kyledinardi <94251418+kyledinardi@users.noreply.github.com>
2024-09-30 11:50:10 -0400

    Allow users to upload images with posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,25 +1,44 @@
-// const multer = require('multer');
+const multer = require('multer');
 const asyncHandler = require('express-async-handler');
 const { body, validationResult } = require('express-validator');
-// const { unlink } = require('fs/promises');
-// const cloudinary = require('cloudinary').v2;
+const { unlink } = require('fs/promises');
+const cloudinary = require('cloudinary').v2;
 const { PrismaClient } = require('@prisma/client');
 
+const storage = multer.diskStorage({
+  destination: 'uploads/',
+
+  filename(req, file, cb) {
+    cb(null, `${crypto.randomUUID()}.${file.originalname.split('.').pop()}`);
+  },
+});
+
+const upload = multer({ storage });
 const prisma = new PrismaClient();
 
 exports.createPost = [
+  upload.single('postImage'),
   body('text', 'Post text must not be empty').trim().notEmpty(),
 
   asyncHandler(async (req, res, next) => {
     const errors = validationResult(req);
     const { text } = req.body;
+    let imageUrl = null;
 
     if (!errors.isEmpty()) {
       return res.status(400).json({ text, errors: errors.array() });
     }
 
+    if (req.file) {
+      if (req.file) {
+        const result = await cloudinary.uploader.upload(req.file.path);
+        imageUrl = result.secure_url;
+        unlink(req.file.path);
+      }
+    }
+
     const post = await prisma.post.create({
-      data: { text, author: { connect: { id: req.user.id } } },
+      data: { text, imageUrl, author: { connect: { id: req.user.id } } },
       include: { author: true },
     });
 
